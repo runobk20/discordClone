a9cf83230047152479cc461abdae8524420b4394
Bruno <lipontaleme@gmail.com>
2022-10-31 02:40:31 -0300

    End version

--- assets/scripts/mobileNavApp.js
+++ assets/scripts/mobileNavApp.js
@@ -1,14 +1,24 @@
+const backdrop = document.getElementById('backdrop');
+const header = document.querySelector('#hero > header');
+const closeNavBtn = document.querySelector('.close-icon');
+
 const hamburgerEl = document.getElementById('hamburger-menu');
 const navEl = document.querySelector('.navContainer');
 const navItems = document.querySelectorAll('.mobile-navItem', '.mobile-navLink');
 const navLinkItems = document.querySelectorAll('#header-nav > a');
 
+function toggleBackdrop() {
+    header.classList.toggle('z-index');
+    backdrop.classList.toggle('visible');
+}
 
-function showMobileNavHandler() {
+function openMobileNavHandler() {
     if(document.body.clientWidth > 1024) {
         return;
     }
     
+    toggleBackdrop();
+
     navEl.classList.toggle('show');
     navItems.forEach(entry => entry.classList.toggle('show'));
     navLinkItems.forEach(item => {
@@ -17,4 +27,21 @@ function showMobileNavHandler() {
     })
 }
 
-hamburgerEl.addEventListener('click', showMobileNavHandler)
+function closeMobileNavHandler() {
+    toggleBackdrop();
+    navEl.classList.toggle('show');
+    navItems.forEach(entry => entry.classList.toggle('show'));
+    navLinkItems.forEach(item => {
+        console.dir(item)
+        if(item.classList.contains('mobile-navItem')) {
+            item.style.display = 'none';
+        } else {
+            item.style.display = 'block';
+        }
+
+    })
+}
+
+hamburgerEl.addEventListener('click', openMobileNavHandler);
+closeNavBtn.addEventListener('click', closeMobileNavHandler);
+backdrop.addEventListener('click', closeMobileNavHandler);
